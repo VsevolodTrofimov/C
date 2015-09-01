0cf99d7d9cbc13e36c9abb7f315031f06c17deb7
VsevolodTrofimov <seva.trofimov2@gmail.com>
2015-09-01 22:37:09 +0300

    0.3
    app ip fix

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ var fs = require("fs");
 var addUser= require("addUser");
 var router = require("router.js");
 var bodyParser = require("body-parser");
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
 var app = express();
 
-var server = app.listen(3000, function () {
+var server = app.listen(server_port, server_ip_address , function () {
 	var host = server.address().address;
 	var port = server.address().port;
-	console.log('C is online at', host, port);
+	 console.log( "Listening on " + server_ip_address + ", server_port " + port );
 });
 
 
